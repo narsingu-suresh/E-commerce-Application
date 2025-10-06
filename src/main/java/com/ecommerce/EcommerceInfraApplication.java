a14ef0f85229a5a57ea7887ca5f372aa65f455de
narsingu-suresh <narsingusuresh6@gmail.com>
2025-10-06 13:46:31 +0530

    Infra steup - docker compose files

---
+++ src/main/java/com/ecommerce/EcommerceInfraApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceInfraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceInfraApplication.class, args);
+	}
+
+}
